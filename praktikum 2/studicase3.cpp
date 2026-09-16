49f753093692db816a7c1d9e820cefb86014d7a3
aryasd-cs <aryawiratmaja27@gmail.com>
2026-09-17 03:48:49 +0700

    kuliah

--- praktikum 2/studicase3.cpp
+++ praktikum 2/studicase3.cpp
@@ -19,15 +19,21 @@ int main(){
         cout << "Masukkan tinggi balok: ";
         cin >> tinggi;
 
-        //RUMUS VOLUME DAN LUAS PERMUKAAN BALOK
         volume = panjang * lebar * tinggi;
         luas_permukaan = 2 * (panjang * lebar + panjang * tinggi + lebar * tinggi);
 
         cout << "============================================================" << endl;
 
-        cout << left << setw(10) << "panjang" << left << setw(10) << "lebar" << left << setw(10) << "tinggi"
-        << left << setw(15) << "volume" << left << setw(20) << "luas_permukaan" << endl;
-        cout << left << setw(10) << panjang << left << setw(10) << lebar << left << setw(10) << tinggi
-        << left << setw(15) << volume << left << setw(20) << luas_permukaan << endl;
+        cout << left << setw(10) << "panjang"
+        << left << setw(10) << "lebar"
+        << left << setw(10) << "tinggi"
+        << left << setw(15) << "volume"
+        << left << setw(20) << "luas_permukaan" << endl;
+        
+        cout << left << setw(10) << panjang
+        << left << setw(10) << lebar
+        << left << setw(10) << tinggi
+        << left << setw(15) << volume
+        << left << setw(20) << luas_permukaan << endl;
         return 0;
 }
